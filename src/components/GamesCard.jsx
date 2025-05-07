ec7068096f8c8e03dd354daf93f690d554d0d602
rashik513 <mdrobiulislamrashikpdf@gmail.com>
2025-05-07 20:55:35 +0600

    change loading spiner

--- src/components/GamesCard.jsx
+++ src/components/GamesCard.jsx
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const GamesCard = ({ game }) => {
   const { name, thumbnail, image, rating, category } = game;
+  const [loaded, setLoaded] = useState(false);
+
   return (
     <div className="rounded-lg shadow-xl overflow-hidden bg-white cursor-pointer hover:scale-102 transition-all duration-400">
-      <div className="w-full h-48 overflow-hidden">
-        <img src={image} alt={name} className="w-full h-full object-center" />
+      <div className="w-full h-48 relative bg-gray-100 overflow-hidden flex items-center justify-center">
+        {!loaded && (
+          <div className="absolute inset-0 flex justify-center items-center">
+            <div className="w-8 h-8 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+          </div>
+        )}
+        <img
+          src={image}
+          alt={name}
+          onLoad={() => setLoaded(true)}
+          className={`w-full h-full object-cover transition-opacity duration-500 ${
+            loaded ? "opacity-100" : "opacity-0"
+          }`}
+        />
       </div>
 
-      {/* Bottom Details Section */}
       <div className="flex p-4 gap-3 items-start">
-        {/* Game Thumbnail */}
         <img
           src={thumbnail}
           alt={`${name} thumbnail`}
           className="w-14 h-14 object-cover rounded-lg"
         />
 
-        {/* Info */}
         <div className="flex flex-col">
           <h2 className="font-semibold text-lg">{name}</h2>
           <p className="text-gray-500 text-sm">{category}</p>
--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -4,7 +4,10 @@ import { ClipLoader } from "react-spinners";
 const LoadingPage = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <ClipLoader color="#08ffa4" size={60} speedMultiplier={2} />
+      <div class="relative">
+        <div class="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
+        <div class="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin"></div>
+      </div>
     </div>
   );
 };
