088915619d459cd5f2ab53861e5061282530707d
rashik513 <mdrobiulislamrashikpdf@gmail.com>
2025-05-07 16:50:34 +0600

    add dynamic title based on route

--- src/layouts/Layouts.jsx
+++ src/layouts/Layouts.jsx
@@ -3,10 +3,12 @@ import { Outlet } from "react-router";
 import Home from "../components/navbar/home/Home";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/Footer";
+import DynamicTitle from "../pages/DynamicTitle";
 
 const Layouts = () => {
   return (
     <div>
+      <DynamicTitle></DynamicTitle>
       <Navbar></Navbar>
       <main className="mb-20">
         <Outlet></Outlet>
---
+++ src/pages/DynamicTitle.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import { useLocation } from "react-router";
+
+const DynamicTitle = () => {
+  const location = useLocation();
+  useEffect(() => {
+    const path = location.pathname;
+    let title = "AppStore";
+    if (path === "/") {
+      title = "Home";
+    } else if (path === "/login") {
+      title = "Login";
+    } else if (path === "/register") {
+      title = "Register";
+    } else if (path === "/profile") {
+      title = "Profile";
+    } else if (path === "/games") {
+      title = "Games";
+    } else if (path === "/games/:id") {
+      title = "Game Details";
+    }
+    document.title = `AppStore - ${title}`;
+  }, [location]);
+  return null;
+};
+
+export default DynamicTitle;
